a2c2cb623acafea284cb1f64fbb3e40983abf73b
joaopaulo-UFS <paulo.rodrigues@dcomp.ufs.br>
2026-08-30 15:59:03 -0300

    Implement account management functions
    Added functions to create an account, deposit, and withdraw.

--- conta.py
+++ conta.py
@@ -1,1 +1,11 @@
-#placeholder
+#account_atributo para atributo da conta.
+def criar_conta(nome, senha, saldo):
+    account_name = nome
+    account_password = senha
+    account_balance = saldo
+
+def realizar_deposito(valor):
+    account_balance += valor
+
+def realizar_saque(valor):
+    account_balance -= valor
