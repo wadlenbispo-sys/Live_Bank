43dbb2765c8e6d882d6c6397b028483e124fff18
wadlenbispo-sys <wadlen.bispo@dcomp.ufs.br>
2026-09-03 11:06:28 -0300

    Fix return statements and add age verification function
    Corrected return statements in user_nome, user_senha, and user_cpf functions. Added user_idade function to verify client's age.

--- cliente.py
+++ cliente.py
@@ -2,13 +2,21 @@
 
 # cada variavel está atribuindo ao usuario seus respectivos valores pessoais
 def user_nome(nome_do_usuario):
-nome_do_usuario = nome
-return nome_do_usuario
+
+   return nome_do_usuario
 
 def user_senha(senha__do_usuario):
-senha_do_usuario = senha
-return senha_do_usuario
+
+  return senha_do_usuario
 
 def user_cpf(cpf_do_usuario):
-cpf_do_usuario = cpf
-return cpf_do_usuario
+
+  return cpf_do_usuario
+  
+# verificação de idade do cliente
+def user_idade(idade_do_cliente):
+  
+  if idade_do_cliente >= 18:
+     return  idade_do_cliente,"idade valida"
+  else :
+     return "idade fora das condições, voce pode ser menor de idade!!
