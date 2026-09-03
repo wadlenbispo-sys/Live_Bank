a0240efbd04c23b9798fbc927db214c87bfa8312
wadlenbispo-sys <wadlen.bispo@dcomp.ufs.br>
2026-09-03 10:40:56 -0300

    Implement user detail retrieval functions
    Added functions to retrieve user details such as name, password, and CPF.

--- cliente.py
+++ cliente.py
@@ -1,8 +1,14 @@
 # dados pessoais do cliente;
 
 # cada variavel está atribuindo ao usuario seus respectivos valores pessoais
+def user_nome(nome_do_usuario):
 nome_do_usuario = nome
+return nome_do_usuario
 
+def user_senha(senha__do_usuario):
 senha_do_usuario = senha
+return senha_do_usuario
 
+def user_cpf(cpf_do_usuario):
 cpf_do_usuario = cpf
+return cpf_do_usuario
