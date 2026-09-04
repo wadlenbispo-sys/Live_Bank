ca6cb9eda3901520676a29bd9f2bcbc9b8b5370f
joaopaulo-UFS <paulo.rodrigues@dcomp.ufs.br>
2026-09-03 23:15:01 -0300

    Fix function parameter names and return message

--- cliente.py
+++ cliente.py
@@ -5,7 +5,7 @@ def user_nome(nome_do_usuario):
 
    return nome_do_usuario
 
-def user_senha(senha__do_usuario):
+def user_senha(senha_do_usuario):
 
   return senha_do_usuario
 
@@ -19,4 +19,4 @@ def user_idade(idade_do_cliente):
   if idade_do_cliente >= 18:
      return  idade_do_cliente,"idade valida"
   else :
-     return "idade fora das condições, voce pode ser menor de idade!!
+     return "idade fora das condições, voce pode ser menor de idade!!"
