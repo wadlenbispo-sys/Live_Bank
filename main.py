e3f195208b214e0cd8d2b30d6c758110edcc3846
joaopaulo-UFS <paulo.rodrigues@dcomp.ufs.br>
2026-09-03 22:37:32 -0300

    Fix user_cpf input position and correct idade syntax

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import conta, cliente
 
 cliente.user_name(input())
-cliente.user_cpf(input())
 cliente.user_senha(input())
+cliente.user_cpf(input())
 cliente.user_idade(int(input())
 conta.criar_conta(input().split(),input(), 0.00)
 conta.realizar_deposito(float(input()))
