e682ceb060fee3602055f7379d6e94269661eabe
joaopaulo-UFS <paulo.rodrigues@dcomp.ufs.br>
2026-09-03 21:58:10 -0300

    Import cliente module and update user input calls

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import conta
+import conta, cliente
 
+cliente.user_name(input())
+cliente.user_cpf(input())
+cliente.user_senha(input())
+cliente.user_idade(int(input())
 conta.criar_conta(input().split(),input(), 0.00)
 conta.realizar_deposito(float(input()))
 conta.realizar_saque(float(input()))
