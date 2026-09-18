67963335d8576a782b5de142cbf9c9b2a972c09d
joaopaulo-UFS <paulo.rodrigues@dcomp.ufs.br>
2026-09-18 08:11:20 -0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import conta, cliente
-
-cliente.user_name(input())
-cliente.user_senha(input())
-cliente.user_cpf(input())
-cliente.user_idade(int(input())
-conta.criar_conta(input().split(),input(), 0.00)
-conta.realizar_deposito(float(input()))
-conta.realizar_saque(float(input()))
