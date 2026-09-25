9c6d6026030cc55cdb5ee55557a50b44363fa6ed
joaopaulo-UFS <paulo.rodrigues@dcomp.ufs.br>
2026-09-24 23:22:40 -0300

    Add option to view bank report and handle transactions

--- menu.py
+++ menu.py
@@ -1,5 +1,5 @@
 #Menu de opções 
-import cadastracao_cliente,cadastracao_agencia,cadastracao_conta
+import cadastracao_cliente,cadastracao_agencia,cadastracao_conta,relatorio
 
 
 def menu_de_opcoes():
@@ -12,6 +12,7 @@ def menu_de_opcoes():
   print('5 - Para fazer um saque, (obs:.só é possivel realizar com conta)')
   print('6 - Para fazer uma transferencia, (obs:.só é possivel com conta)')
   print('7 - Para sair do banco')
+  print('8 - Para ver o relatório do banco')
 
 
   opcao = 0
@@ -28,8 +29,26 @@ def menu_de_opcoes():
       if opcao == 3:
          cadastracao_conta.cadastracao_contas(input(),int(input()),int(input()))
          cadastracao_conta.salvar_dados()
-         
-      elif opcao not in [1,2,3,4,5,6,7]:
+
+      if opcao == 4:
+         resultado = cadastracao_conta.deposito(input('Número da conta: '),float(input('Valor do depósito: ')))
+         print(resultado)
+         cadastracao_conta.salvar_dados()
+
+      if opcao == 5:
+         resultado = cadastracao_conta.saque(input('Número da conta: '),float(input('Valor do saque: ')))
+         print(resultado)
+         cadastracao_conta.salvar_dados()
+
+      if opcao == 6:
+         resultado = cadastracao_conta.transferencia(input('Número da conta de origem: '),input('Número da conta de destino: '),float(input('Valor da transferência: ')))
+         print(resultado)
+         cadastracao_conta.salvar_dados()
+
+      if opcao == 8:
+         relatorio.gerar_relatorio_completo()
+
+      elif opcao not in [1,2,3,4,5,6,7,8]:
         print('desculpe para prossequir precisa cadastra')
 
   print('Volte logo, Até mais!')
